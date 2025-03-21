bf5b6a6d68df5fb74207021ca05132b5d05bd2c9
Muhammed Doğancan Yılmazoğlu <muhammed.dogancan.yilmazoglu1@huawei.com>
2025-03-21 08:21:52 +0300

    implement material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-function App() {
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
+import AlarmIcon from "@mui/icons-material/Alarm";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+
+function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800 font-[system-ui]" >
-      Coming Soon
-      </h1>
+      <Card sx={{ maxWidth: 450 }}>
+        <CardContent className="flex  items-center gap-4 !py-12 !px-12">
+          <h1 className="text-4xl font-bold text-gray-800 font-[system-ui]">
+            Coming Soon
+          </h1>
+          <AlarmIcon sx={{ fontSize: 40 }} className="mt-2" />
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
