62652a6904e0357a9dda1d54bb6b6bf410f8432a
Muhammed Doğancan Yılmazoğlu <muhammed.dogancan.yilmazoglu1@huawei.com>
2025-03-24 13:40:09 +0300

    improve coming soon card

--- src/components/Fragments/cards/ComingSoon.tsx
+++ src/components/Fragments/cards/ComingSoon.tsx
@@ -1,15 +1,17 @@
-import { Card, CardContent } from "@mui/material"
-import AlarmIcon from "@mui/icons-material/Alarm"
+import { Card, CardContent } from "@mui/material";
+import AlarmIcon from "@mui/icons-material/Alarm";
 
 const ComingSoon = () => {
-    return (<Card className="w-[450px]">
-    <CardContent className="flex justify-center bg-background-dark border border-primary-border-dark items-center gap-4 !py-12 !px-12">
-      <h1 className="text-4xl font-bold text-primary font-[system-ui]">
-        Coming Soon
-      </h1>
-      <AlarmIcon sx={{ fontSize: 40 }} className="mt-2 text-primary" />
-    </CardContent>
-  </Card>)
-} 
+  return (
+    <Card className="w-[450px]">
+      <CardContent className="flex justify-center bg-background-dark border border-primary-border-dark items-center gap-4 !py-12 !px-12">
+        <h1 className="text-4xl font-bold text-primary font-tech-mono">
+          Coming Soon
+        </h1>
+        <AlarmIcon sx={{ fontSize: 40 }} className="text-primary" />
+      </CardContent>
+    </Card>
+  );
+};
 
-export default ComingSoon
+export default ComingSoon;
