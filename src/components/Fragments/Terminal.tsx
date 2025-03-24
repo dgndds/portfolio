c97b393df3795f83059e1681849dae3a7ab94abc
Muhammed Doğancan Yılmazoğlu <muhammed.dogancan.yilmazoglu1@huawei.com>
2025-03-24 13:11:40 +0300

    improve terminal component design

--- src/components/Fragments/Terminal.tsx
+++ src/components/Fragments/Terminal.tsx
@@ -6,19 +6,20 @@ import HorizontalRuleIcon from "@mui/icons-material/HorizontalRule";
 interface TerminalProps {
   className?: string;
   title: string;
+  children?: React.ReactNode;
 }
 
 //shadow-[0px_0px_5px_2.5px_rgba(16,137,99,0.75)]
 
-const Terminal = ({ className, title }: TerminalProps) => {
+const Terminal = ({ className, title, children }: TerminalProps) => {
   return (
     <div
       className={twMerge(
-        "bg-[#151515] border-2 border-primary-border",
+        "bg-[#151515] border-2 border-primary-border h-full",
         className
       )}
     >
-      <div className="h-9 bg-black flex items-center justify-between px-2 font-tech-mono font-bold text-[#efefef] text-[12px]">
+      <div className="h-9 bg-primary-border-dark flex items-center justify-between px-2 font-tech-mono font-bold text-[#efefef] text-[12px]">
         {title}
 
         <div className="flex gap-2">
@@ -28,8 +29,15 @@ const Terminal = ({ className, title }: TerminalProps) => {
         </div>
       </div>
 
-      <div className="text-white p-2 font-tech-mono ">
-        {"> npm run install"}<span className="animate-blink">_</span>
+      <div className="text-[#ffffffc2] p-2 font-tech-mono ">
+        {children ? (
+          children
+        ) : (
+          <>
+            {"> npm run install"}
+            <span className="animate-blink">_</span>
+          </>
+        )}
       </div>
     </div>
   );
